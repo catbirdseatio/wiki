d0fb6a6df635c4ec65d227de0e5e3f96e169dd8d
Leigh Michael Forrest <leigh@catbirdseat.io>
2023-06-19 11:24:47 -0400

    search functionality added

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.title, name="title"),
+    path("search/<str:q>", views.search, name="search"),
+    path("wiki/<str:title>", views.title, name="title"),
+    path("notfound", views.not_found, name="not_found"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,36 @@
+import re
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
+    q = request.GET.get("q")
+    entries = util.list_entries()
+
+    if q:
+        if q in entries:
+            return HttpResponseRedirect(reverse("title", args=[q]))
+        return HttpResponseRedirect(reverse("search", args=[q]))
+    else:
+        return render(request, "encyclopedia/index.html", {"entries": entries})
+
+
+def search(request, q):
+    entries = util.list_entries()
+    results = [entry for entry in entries if re.search(q, entry, re.IGNORECASE)]
+    return render(request, "encyclopedia/search.html", {
+        "results": results
+    })
 
 
 def title(request, title):
     entry = util.get_entry(title)
-    return render(request, "encyclopedia/entry.html", {"entry": entry})
+    if entry:
+        return render(request, "encyclopedia/entry.html", {"entry": entry})
+    return HttpResponseRedirect(reverse("not_found"))
+
+def not_found(request):
+    return render(request, "encyclopedia/not_found.html")
