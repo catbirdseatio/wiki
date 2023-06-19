8a99e669e04627ec1ab10a9d5af37e9638e3e8e0
Leigh Michael Forrest <leigh@catbirdseat.io>
2023-06-19 09:50:44 -0400

    changed markdown library

--- encyclopedia/templatetags/markdown.py
+++ encyclopedia/templatetags/markdown.py
@@ -1,12 +1,13 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 
-import markdown as md
+from markdown2 import Markdown
 
 register = template.Library()
+md = Markdown()
 
 
 @register.filter()
 @stringfilter
 def markdown(value):
-    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
+    return md.convert(value)
